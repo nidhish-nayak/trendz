ebd468fe09be6181967f63dee3fc42060dd7ee04
nidhish-nayak <nidhibelthangady@gmail.com>
2024-03-09 15:42:14 +0530

    Adding vitest to client for UT, Integration & E2E Testing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    test: {
+        includeSource: ["src/**/*.{js,ts}"],
+    },
+    define: {
+        "import.meta.vitest": "undefined",
+    },
 });
