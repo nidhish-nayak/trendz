65cc88ea1e6fd57f1b6b633dff346198000653cb
nidhish-nayak <nidhibelthangady@gmail.com>
2024-03-15 21:18:57 +0530

    Added post sharing feature - testing pending

--- client/src/components/share/web/webShare.tsx
+++ client/src/components/share/web/webShare.tsx
@@ -1,10 +1,62 @@
 import { ShareOutlined } from "@mui/icons-material";
+import { useEffect, useState } from "react";
 import config from "../../../config/config";
 import { PostTypes } from "../../post/post.types";
 import "./webShare.scss";
 
 const WebShare = ({ post }: PostTypes) => {
-    const { id, desc } = post;
+    const { id, name, desc, img } = post;
+    const [postTitle, setPostTitle] = useState("Trendz - Social Media");
+    const [postDescription, setPostDescription] = useState(
+        "Welcome to Trendz, where our vibrant communities thrive!"
+    );
+    const [postImageUrl, setPostImageUrl] = useState(
+        "https://github.com/nidhish-nayak/trendz/assets/76598208/a40c20aa-fc1e-4899-a693-b7db80a98296"
+    );
+
+    const handleShareClick = async () => {
+        setPostTitle(`${name}'s Post`);
+        setPostDescription(desc);
+        if (img) {
+            setPostImageUrl(img);
+        }
+
+        // Wait for state updates to complete
+        await new Promise((resolve) => setTimeout(resolve));
+
+        // Call sharePost after state updates are complete
+        sharePost();
+    };
+
+    useEffect(() => {
+        // Update Open Graph meta tags in the document's <head> section
+        const updateMetaTags = async () => {
+            if (!document) return; // Ensure document is not null or undefined
+
+            const ogTitleMeta = document.querySelector(
+                'meta[property="og:title"]'
+            );
+            if (ogTitleMeta) {
+                ogTitleMeta.setAttribute("content", postTitle);
+            }
+
+            const ogDescriptionMeta = document.querySelector(
+                'meta[property="og:description"]'
+            );
+            if (ogDescriptionMeta) {
+                ogDescriptionMeta.setAttribute("content", postDescription);
+            }
+
+            const ogImageMeta = document.querySelector(
+                'meta[property="og:image"]'
+            );
+            if (ogImageMeta) {
+                ogImageMeta.setAttribute("content", postImageUrl);
+            }
+        };
+
+        updateMetaTags();
+    }, [postTitle, postDescription, postImageUrl]);
 
     const sharePost = () => {
         // Use the Web Share API if supported
@@ -23,7 +75,7 @@ const WebShare = ({ post }: PostTypes) => {
     };
 
     return (
-        <span className="share-web" onClick={sharePost}>
+        <span className="share-web" onClick={handleShareClick}>
             <ShareOutlined />
         </span>
     );
