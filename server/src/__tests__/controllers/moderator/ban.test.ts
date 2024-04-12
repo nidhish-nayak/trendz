c2b487302fac63525aae45fa5ec5e58b85003072
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-12 15:07:05 +0530

    Updated CI/CD .env, refactored ban tests

--- server/src/__tests__/controllers/moderator/ban.test.ts
+++ server/src/__tests__/controllers/moderator/ban.test.ts
@@ -34,7 +34,11 @@ describe("Ban moderator test", () => {
         app.use("/api/posts", postRoutes);
         app.use("/api/stories", storyRoutes);
 
+        // Register the user before test
         await request(app).post("/api/auth/register").send(userAlt);
+    });
+
+    it("responds 200 for user login", async () => {
         const { userId, token } = await manualAuth(
             app,
             "/api/auth/login",
@@ -45,7 +49,11 @@ describe("Ban moderator test", () => {
             return console.log("Login failed - no token returned!");
         user_id = userId;
         accessToken = token;
+        expect(user_id).toBeDefined();
+        expect(accessToken).toBeDefined();
+    });
 
+    it("adds a new post for mocking ban scenario", async () => {
         const { data, error } = await supabase
             .from("posts")
             .insert({
@@ -56,55 +64,71 @@ describe("Ban moderator test", () => {
             .select();
         if (error) return console.log("Post upload to DB has failed!");
         post_id = data[0].id;
+        expect(post_id).toBeDefined();
     });
 
-    it("responds 200 for posts ban success", async () => {
-        // Call moderator - Main test
-        const res = await moderatorCheck(post_id, "posts", user_id, imgUrl);
-        expect(res).toBeUndefined();
+    it("bans the user after posting post with unsafe image", async () => {
+        // Ban user after posting imgUrl
+        await expect(
+            moderatorCheck(post_id, "posts", user_id, imgUrl)
+        ).resolves.toBeUndefined();
 
+        // Verify user is banned and not able to login
         const login = await request(app)
             .post("/api/auth/login")
             .send(credentials);
         expect(login.status).toBe(401);
+    });
 
-        // // Remove banned user
-        // const { error: banError } = await supabase
-        //     .from("bans")
-        //     .delete()
-        //     .eq("user_id", user_id);
-        // if (banError) return console.log("User unban failed!");
-        //
-        // const { data, error } = await supabase
-        //     .from("stories")
-        //     .insert({
-        //         img: imgUrl,
-        //         userId: user_id,
-        //     })
-        //     .select();
-        // if (error) return console.log("Story upload to DB has failed!");
-        //
-        // // Call moderator - Main test
-        // await moderatorCheck(data[0].id, "stories", user_id, imgUrl);
-        //
-        // const res = await request(app)
-        //     .post("/api/auth/login")
-        //     .send(credentials);
-        // expect(res.status).toBe(401);
-        //
-        // const removeBan = await supabase
-        //     .from("bans")
-        //     .delete()
-        //     .eq("user_id", user_id);
-        // expect(removeBan.status).toBe(204);
-        //
-        // const { status } = await manualAuth(
-        //     app,
-        //     "/api/auth/deregister",
-        //     credentials,
-        //     accessToken
-        // );
-        // expect(status).toBe(200);
+    it("removes the user from banned records", async () => {
+        // Remove banned user
+        const removeBan = await supabase
+            .from("bans")
+            .delete()
+            .eq("user_id", user_id);
+        expect(removeBan.status).toBe(204);
+    });
+
+    it("adds a new story for mocking ban scenario", async () => {
+        const { data, error } = await supabase
+            .from("stories")
+            .insert({
+                img: imgUrl,
+                userId: user_id,
+            })
+            .select();
+        if (error) return console.log("Story upload to DB has failed!");
+        story_id = data[0].id;
+        expect(story_id).toBeDefined();
+    });
+
+    it("bans the user after posting story with unsafe image", async () => {
+        // Ban user after posting imgUrl
+        await expect(
+            moderatorCheck(story_id, "stories", user_id, imgUrl)
+        ).resolves.toBeUndefined();
+
+        // Verify user is banned and not able to login
+        const login = await request(app)
+            .post("/api/auth/login")
+            .send(credentials);
+        expect(login.status).toBe(401);
+    });
+
+    it("cleanup - remove ban & deregister test user", async () => {
+        const removeBan = await supabase
+            .from("bans")
+            .delete()
+            .eq("user_id", user_id);
+        expect(removeBan.status).toBe(204);
+
+        const { status } = await manualAuth(
+            app,
+            "/api/auth/deregister",
+            credentials,
+            accessToken
+        );
+        expect(status).toBe(200);
     });
 
     afterAll(() => {
